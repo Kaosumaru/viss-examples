c88a8f141263198c8c6df54121357d08af46315c
Mateusz Borycki <mateuszborycki@gmail.com>
2025-09-30 00:03:43 +0200

    Update

--- src/glsl/includes/spiral.viss.glsl
+++ src/glsl/includes/spiral.viss.glsl
@@ -7,23 +7,7 @@ uniform vec2 uResolution;
 
 varying vec2 vUv;
 
-#define PI 3.14159265358979323846
 
-#pragma editor: preview, export
-float add(float a, float b) {
-  return a + b;
-}
-
-#pragma editor: preview, export
-float spiral(vec2 uv, float time, float dis, float width, float blur)
-{
-    vec2 o=uv;
-    float angle=atan(o.y,o.x);
-    float l=length(o);
-    float offset=l+(angle/(2.*PI))*dis;
-    float circles=mod(offset-time,dis);
-    return (smoothstep(circles-blur,circles,width)-smoothstep(circles,circles+blur,width));
-}
 
 void main() {
   float _glslFunction_94a8b5e4_d393_4255_bfd3_992d5eeff603_0 = spiral(vUv, uTime, 0.1, 0.1, 0.1);
